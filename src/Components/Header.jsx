641b26d93d9fbbfb00f0e99dd949874ae1ecab8c
laurilako <konsta.laurila1@gmail.com>
2023-11-02 16:49:10 +0200

    Remove boilerplate, add header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Header.css';
+
+const Header = () => {
+    return (
+        <div className='Header'>
+            <h1>Servicebook</h1>
+        </div>
+    );
+};
+
+export default Header;
